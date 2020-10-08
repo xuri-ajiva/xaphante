00019ee2184b74b47c956a50daf8757bbaae2b40
xuri02 <t0bl4s.durst@gmail.com>
2020-10-08 21:42:45 +0200

    [+] separate input/output xml controls

---
+++ xaphante/xaphante-model-exporter-interface/InputFileControl.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using Path = System.IO.Path;
+
+namespace xaphante_model_exporter_interface
+{
+    /// <summary>
+    /// Interaction logic for InputFileControl.xaml
+    /// </summary>
+    public partial class InputFileControl
+    {
+        public event Action<string> OnOpen;
+
+        public InputFileControl()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenInput(object sender, RoutedEventArgs e)
+        {
+            var of = new OpenFileDialog();
+            if (of.ShowDialog() != true)
+                return;
+
+            this.inputLBox.Text = of.FileName;
+            this.OnOpen?.Invoke(of.FileName);
+        }
+
+        private void ShowInput(object sender, RoutedEventArgs e)
+        {
+            App.ExplorerShow(this.inputLBox.Text);
+        }
+    }
+}
---
+++ xaphante/xaphante-model-exporter-interface/OutputFileControl.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace xaphante_model_exporter_interface
+{
+    /// <summary>
+    /// Interaction logic for OutputDileControl.xaml
+    /// </summary>
+    public partial class OutputFileControl : UserControl
+    {
+        public event Action<string> OnOpen;
+
+        public OutputFileControl()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenOutput(object senOpenOutputEventArgs, RoutedEventArgs e)
+        {
+            var sf = new SaveFileDialog { DefaultExt = this.EXTENSION, AddExtension = true };
+            if (sf.ShowDialog() != true)
+                return;
+
+            this.outputLBox.Text = sf.FileName;
+        }
+
+        private void ShowOutput(object sender, RoutedEventArgs e)
+        {
+            App.ExplorerShow(this.outputLBox.Text);
+        }
+
+        public string EXTENSION { get; set; }
+    }
+}
